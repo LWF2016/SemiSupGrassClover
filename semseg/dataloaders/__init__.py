43e757e86e7a2904afec2a2109a60979eb27f052
LWF2016 <A1432529196@outlook.com>
2025-12-01 22:52:41 +0800

    Update __init__.py

--- semseg/dataloaders/__init__.py
+++ semseg/dataloaders/__init__.py
@@ -8,6 +8,8 @@ def make_data_loader(args, **kwargs):
         train_set = danish.CloverSegmentation(args, split='train')
         val_set = danish.CloverSegmentation(args, split='val')
         num_class = train_set.NUM_CLASSES
+        print('train_loader')
+        print(train_loader)
         train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
         test_loader = None
