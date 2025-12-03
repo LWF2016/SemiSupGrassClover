bdf410e96f639670d6a47ad6f73f85d45f774743
LWF2016 <A1432529196@outlook.com>
2025-12-03 10:48:53 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 mkdir logs
-CUDA_VISIBLE_DEVICES=0 python main.py --net resnet18 --dataset irish --epochs 100 --steps 50 80 --batch-size 16 --save-dir clover_res18_irish --seed 1 --lr 0.03 --num-classes 4 --pretrained imagenet > logs/logs_irish.txt
-CUDA_VISIBLE_DEVICES=0 python main.py --net resnet18 --dataset irish_ext --epochs 100 --steps 50 80 --batch-size 16 --save-dir clover_res18_irish_ext --seed 1 --lr 0.03 --num-classes 4 --pretrained imagenet > logs/logs_irish_ext.txt
+CUDA_VISIBLE_DEVICES=0 python main.py --net resnet18 --dataset irish --epochs 5 --steps 50 80 --batch-size 16 --save-dir clover_res18_irish --seed 1 --lr 0.03 --num-classes 4 --pretrained imagenet > logs/logs_irish.txt
+CUDA_VISIBLE_DEVICES=0 python main.py --net resnet18 --dataset irish_ext --epochs 5 --steps 50 80 --batch-size 16 --save-dir clover_res18_irish_ext --seed 1 --lr 0.03 --num-classes 4 --pretrained imagenet > logs/logs_irish_ext.txt
 
-CUDA_VISIBLE_DEVICES=0 python main.py --net resnet18 --dataset danish --epochs 100 --steps 50 80 --batch-size 16 --save-dir clover_res18_danish --seed 1 --lr 0.03 --num-classes 5 --pretrained imagenet > logs/logs_danish.txt
-CUDA_VISIBLE_DEVICES=0 python main.py --net resnet18 --dataset danish_ext --epochs 100 --steps 50 80 --batch-size 16 --save-dir clover_res18_danish_ext --seed 1 --lr 0.03 --num-classes 5 --pretrained imagenet > logs/logs_danish_ext.txt
+# CUDA_VISIBLE_DEVICES=0 python main.py --net resnet18 --dataset danish --epochs 5 --steps 50 80 --batch-size 16 --save-dir clover_res18_danish --seed 1 --lr 0.03 --num-classes 5 --pretrained imagenet > logs/logs_danish.txt
+# CUDA_VISIBLE_DEVICES=0 python main.py --net resnet18 --dataset danish_ext --epochs 5 --steps 50 80 --batch-size 16 --save-dir clover_res18_danish_ext --seed 1 --lr 0.03 --num-classes 5 --pretrained imagenet > logs/logs_danish_ext.txt
